f5e4e8cf3b04ecbf289301cb0f286383ea334fff
Zakaria09 <92021986+ZakariaFichtali@users.noreply.github.com>
2023-01-02 10:23:12 +0100

    DOM Done

---
+++ DOM/DOM/Exercises 01/index01.js
@@ -0,0 +1,6 @@
+document.getElementById('username')
+    .insertAdjacentHTML('beforebegin','<label for="username">Username : </label> <br>');
+document.getElementById('password')
+    .insertAdjacentHTML('beforebegin','<label for="password">Password :</label><br>');
+document.getElementById('confirmPassword')
+    .insertAdjacentHTML('beforebegin','<label for="confirmPassword">Confirm Password :</label><br>');
---
+++ DOM/DOM/Exercises 02/index01.js
@@ -0,0 +1,10 @@
+const img1 = document.getElementById('img1');
+img1.src = "https://i.stack.imgur.com/RQ3Id.png";
+img1.style.height = "100px";
+
+const h1 = document.getElementById('h1');
+h1.innerText = "Hello";
+
+// const img0 = document.createElement("img");
+
+
